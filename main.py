1c38d355f49bc1db6da32b77b7859f45d98a4674
RISS0991 <polinakirej9@gmail.com>
2026-06-09 12:25:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from kivy.uix.button import Button
 class MainApp(App):
     def build(self):
         layout = BoxLayout(orientation='vertical')
-        self.label = Label(text='Привет, Android!', font_size='24sp')
+        self.label = Label(text='Работа не волк, никто не волк, только волк волк', font_size='24sp')
         layout.add_widget(self.label)
-        btn = Button(text='Нажми меня')
+        btn = Button(text='Кнопка не волк')
         btn.bind(on_press=self.on_button_press)
         layout.add_widget(btn)
         return layout
     
     def on_button_press(self, instance):
-        self.label.text = 'Кнопка нажата!'
+        self.label.text = 'Волк не кнопка'
 
 if __name__ == '__main__':
     MainApp().run()
