e0bf3a322f06316be939e611684420b9bdfad4ac
RISS0991 <polinakirej9@gmail.com>
2026-06-09 12:23:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
+class MainApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
+        self.label = Label(text='Привет, Android!', font_size='24sp')
+        layout.add_widget(self.label)
+        btn = Button(text='Нажми меня')
+        btn.bind(on_press=self.on_button_press)
+        layout.add_widget(btn)
+        return layout
+    
+    def on_button_press(self, instance):
+        self.label.text = 'Кнопка нажата!'
+
+if __name__ == '__main__':
+    MainApp().run()
